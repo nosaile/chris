0da6993051765672750f0049fbd67909e922e787
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-07-12 15:19:14 -0500

    creating portfolio using react

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import './Navbar.css'
+
+const Navbar= () => {
+
+const [click, setClick] = useState(false);
+const handleClick = () => setClick(!click);
+
+  return (
+    <div className='navbar'>
+        <div className='container'>
+            <h1 style={{marginLeft: '1rem', color: 'cyan'}}>chris.</h1>
+            <ul className={click ? 'nav active' : 'nav'}>
+                <li className='nav-item'>
+                    <a className='btn' href="/">home</a>
+                </li>
+                <li className='nav-item'>
+                    <a className='btn' href="/">about</a>
+                </li>
+                <li className='nav-item'>
+                    <a className='btn' href="/">projects</a>
+                </li>
+                <li className='nav-item'>
+                    <a className='btn' href="/">contact</a>
+                </li>
+            </ul>
+            <div onClick={handleClick} className='hamburger'>
+                {click ? <AiOutlineClose className='icon' /> : <AiOutlineMenu className='icon'/>} 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
