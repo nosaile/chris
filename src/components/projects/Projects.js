e53ed83513e8da2498a20ee7be2b0d0670099612
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-07-14 21:31:28 -0500

    adding project pictures

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -7,21 +7,46 @@ const Projects = () => {
             <div className='container'>
                 <div className='vetted'>
                     <h1>vetted</h1>
+                    <div className='project-galllery'>
+
+                    </div>
+                    <p className='project-content'>
+                        Vetted is an audio, video, and text communication platform that links veterans
+                        with shared experiences and interests in an online environment with consistency,
+                        compassion, and connection as a priority. Through identity verification and complete
+                        anonymity, we are able to provide veterans with a protected place to communicate whenever
+                        they want, wherever they want, and how they want.
+                    </p>
                 </div>
                 <div className='weather-map'>
                     <h1>weather map</h1>
+                    <p className='project-content'>
+
+                    </p>
                 </div>
                 <div className='contacts-manager'>
                     <h1>contacts manager</h1>
+                    <p className='project-content'>
+
+                    </p>
                 </div>
                 <div className='adventure'>
                     <h1>appalachian legends</h1>
+                    <p className='project-content'>
+
+                    </p>
                 </div>
                 <div className='movies-api'>
                     <h1>movies api</h1>
+                    <p className='project-content'>
+
+                    </p>
                 </div>
                 <div className='weather-react'>
                     <h1>weather react</h1>
+                    <p className='project-content'>
+
+                    </p>
                 </div>
             </div>
         </div>
