ed12d2dac816d786e2740830c3fd04497151a5e5
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-07-14 22:49:22 -0500

    added links to projects pictures, need to add links to the active web pages laterand format the buttons correctly for mobile viewing

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,52 +1,32 @@
 import React from 'react'
 import './Projects.css'
 
+
 const Projects = () => {
     return (
         <div id='projects' className='projects'>
             <div className='container'>
-                <div className='vetted'>
-                    <h1>vetted</h1>
-                    <div className='project-galllery'>
-
+                <div className='first-line'>
+                    <div className='project-link'>
+                        <p><a target='_blank' href='https://www.flickr.com/photos/196043505@N05/albums/72177720300545991'>vetted</a></p>
                     </div>
-                    <p className='project-content'>
-                        Vetted is an audio, video, and text communication platform that links veterans
-                        with shared experiences and interests in an online environment with consistency,
-                        compassion, and connection as a priority. Through identity verification and complete
-                        anonymity, we are able to provide veterans with a protected place to communicate whenever
-                        they want, wherever they want, and how they want.
-                    </p>
+                    <div className='project-link'>
+                        <p><a target='_blank' href='https://www.flickr.com/photos/196043505@N05/albums/72177720300557048'>weather map</a></p>
+                    </div>
+                    <div className='project-link'>
+                        <p><a target='_blank' href='https://www.flickr.com/photos/196043505@N05/albums/72177720300555539'>contacts manager</a></p>
+                    </div>
                 </div>
-                <div className='weather-map'>
-                    <h1>weather map</h1>
-                    <p className='project-content'>
-
-                    </p>
-                </div>
-                <div className='contacts-manager'>
-                    <h1>contacts manager</h1>
-                    <p className='project-content'>
-
-                    </p>
-                </div>
-                <div className='adventure'>
-                    <h1>appalachian legends</h1>
-                    <p className='project-content'>
-
-                    </p>
-                </div>
-                <div className='movies-api'>
-                    <h1>movies api</h1>
-                    <p className='project-content'>
-
-                    </p>
-                </div>
-                <div className='weather-react'>
-                    <h1>weather react</h1>
-                    <p className='project-content'>
-
-                    </p>
+                <div className='second-line'>
+                    <div className='project-link'>
+                        <p><a target='_blank' href='https://www.flickr.com/photos/196043505@N05/albums/72177720300547545'>text-based game</a></p>
+                    </div>
+                    <div className='project-link'>
+                        <p><a target='_blank' href='https://www.flickr.com/photos/196043505@N05/albums/72177720300546001'>movies api</a></p>
+                    </div>
+                    <div className='project-link'>
+                        <p><a target='_blank' href='https://www.flickr.com/photos/196043505@N05/albums/72177720300557103'>weather app react</a></p>
+                    </div>
                 </div>
             </div>
         </div>
