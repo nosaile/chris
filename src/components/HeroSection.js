0c00b16cd145e5336de1aa5194969c36ecb1a2bb
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-09-07 16:09:30 -0500

    more content in aboutme

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,15 +10,16 @@ import './HeroSection.css';
 // var aboutMe = &quot;scroll down to learn about me&quot;
 // &rbrace;
 
-function HeroSection() {
+function HeroSection(props) {
 
     const el = useRef(null);
     const e = useRef(null);
     const l = useRef(null);
+    const q = useRef(null);
 
 useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ["function whoIsChrisEliason( ) &#123;\n"],
+      strings: ["function whoIsChrisEliason( ) &#123;"],
       typeSpeed: 70,
       smartBackspace: true,
       loop: false,
@@ -26,28 +27,41 @@ useEffect(() => {
     });
 
     const typed2 = new Typed(e.current, {
-        strings: ["var aboutMe = &quot;scroll down to learn about me&quot;;"],
+        strings: ["const person = {<br />firstName: &quot;Christopher&quot;,<br />lastName: &quot;Eliason&quot;,<br />hobbies: [ &quot;Mountain Biking&quot; ,  &quot;Video Games&quot; ,  &quot;Eating Mexican Food&quot; ],<br />age: 26"],
         startDelay: 3200,
         typeSpeed: 70,
-        backSpeed: 100,
+        backSpeed: 10,
         smartBackspace: true,
         loop: true,
         showCursor: false,
     });
+   
 
     const typed3 = new Typed(l.current, {
-        strings: ["\n&rbrace;"],
-        startDelay: 7850,
+        strings: ["&rbrace;"],
+        startDelay: 17000,
         typeSpeed: 70,
         smartBackspace: true,
         loop: false,
         showCursor: false,
     });
 
+    const typed4 = new Typed(q.current, {
+        strings: ["var skills = [ &quot;Javascript&quot; , &quot;Java&quot; , &quot;HTML&quot; , &quot;CSS&quot;]"],
+        typeSpeed: 70,
+        startDelay: 18000,
+        smartBackspace: true,
+        loop: false,
+        showCursor: false,
+      });
+
+
+
     return () => {
         typed.destroy();
         typed2.destroy();
         typed3.destroy();
+        typed4.destroy();
       };
     }, []);
 
@@ -59,6 +73,7 @@ useEffect(() => {
             <span className='func' ref={el}></span>
             <span className='var' ref={e}></span>
             <span className='curly' ref={l}></span>
+            <span className='skills' ref={q}></span>
         </div>
 
 
