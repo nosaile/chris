28074e08e3f96af9625eb1ea71e77f136fdfa644
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-09-09 14:07:16 -0500

    projects tab mobile ready

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -14,10 +14,13 @@ function ProjectPage() {
                 </div>
             </div>
             <div className="project-box">
+                <div className="project-description no">
+                    A dynamic map that allows a user to pinpoint any location on earth and get weather data in a 5-day forecast for that area, using data grabbed from several APIs. Skills gained were the proper usage of and how to repurpose existing API data for custom formatting.
+                </div>
                 <div className="project-img">
                     <img src="../../assets/weather.png" alt="weather map homepage"></img>
                 </div>
-                <div className="project-description">
+                <div className="project-description hide">
                     A dynamic map that allows a user to pinpoint any location on earth and get weather data in a 5-day forecast for that area, using data grabbed from several APIs. Skills gained were the proper usage of and how to repurpose existing API data for custom formatting.
                 </div>
             </div>
@@ -28,17 +31,23 @@ function ProjectPage() {
                 <div className="project-img">
                     <img src="../../assets/appalachian.png" alt="appalachian adventure game"></img>
                 </div>
+                <div className="project-description hide no">
+                    An adventure game. The point is to try and survive throughout the night in the Appalachian mountains. However you will encounter many monstrous creatures who will attempt to harm you. Can you survive the night?
+                </div>
             </div>
             <div className="project-box">
+                <div className="project-description no">
+                    An app made to store a collection of movie data, as well as allow the end-user to enter new information. In this project, I explored grabbing data from an established database to bring it in to our own format. My role was on the usage of the API and bringing our data to the front-end user experience.
+                </div>
                 <div className="project-img">
                     <img src="../../assets/movie.png" alt="movie app homepage"></img>
                 </div>
-                <div className="project-description">
+                <div className="project-description hide">
                     An app made to store a collection of movie data, as well as allow the end-user to enter new information. In this project, I explored grabbing data from an established database to bring it in to our own format. My role was on the usage of the API and bringing our data to the front-end user experience.
                 </div>
             </div>
             <div className="function-call">
-              <Link to="/about" className='func'><h1>whoIsChrisEliason();</h1></Link>
+                <Link to="/about" className='func'><h1>whoIsChrisEliason();</h1></Link>
             </div>
         </div>
     )
